41c917e9008143df1ef6115edbc4fbf47338324c
Muffinney <mf101408@wgcloud.org>
2023-11-17 11:28:40 -0600

    t

--- Turkey.py
+++ Turkey.py
@@ -1,7 +1,19 @@
+cookTim = []
+def convertTim():
+    if min >= 60:
+        hour = (min-(min%60))/60
+        min = min%60
+        if hour >= 24:
+            day = (hour-(hour%24))/24
+            hour = hour%24
+            if day >= 7:
+                week = (day-(day%7))/7
+                day = day%7
 while True:
+    min = 0
     hour = 0
     day = 0
-    min = 0
+    week = 0
     # ask number of turkeys
     numTur = int(input("Number of turkeys: "))
     for i in range(numTur):
@@ -10,15 +22,15 @@ while True:
         method = input("How do you want to cook the turkey? (roast, grill, fry, smoke, or boil) ")
         # calculate cooking time based on method of cooking
         if method == "roast":
-            min += weight * 14
+            indivTim = weight * 14
         elif method == "grill":
-            min += weight * 12
+            indivTim = weight * 12
         elif method == "fry":
-            min += weight * 4
+            indivTim = weight * 4
         elif method == "smoke":
-            min += weight * 35
+            indivTim = weight * 35
         elif method == "boil":
-            min += weight * 13
+            indivTim = weight * 13
         else:
             print("That method of cooking is not supported by this program")
             # ask if user wants to run program again
@@ -28,15 +40,29 @@ while True:
                 continue
             else:
                 break
-    # convert minutes to hours and days
-    if min > 60:
-        hour = (min-min%60)/60
-        min = min%60
-        if hour > 24:
-            day = (hour-hour%24)/24
-            hour = hour%24
-    # print times
-    if day > 0:
+        cookTim.append(indivTim)
+    for i in range(numTur):
+        min = cookTim[i]
+        convertTim()
+        if week > 0:
+            print("Turkey " +  is " + str(week) + " weeks, " + str(day) + " days, " + str(hour) + " hours, " + str(min) + " minutes")
+        elif day > 0:
+            print("Your total cooking time is " + str(day) + " days, " + str(hour) + " hours, " + str(min) + " minutes")
+        elif hour > 0:
+            print("Your total cooking time is " + str(hour) + " hours, " + str(min) + " minutes")
+        else:
+            print("Your total cooking time is " + str(min) + " minutes")
+        min = 0
+        hour = 0
+        day = 0
+        week = 0
+    # sum individual cook times
+    min = sum(cookTim)
+    convertTim()
+    # print total cook time
+    if week > 0:
+        print("Your total cooking time is " + str(week) + " weeks, " + str(day) + " days, " + str(hour) + " hours, " + str(min) + " minutes")
+    elif day > 0:
         print("Your total cooking time is " + str(day) + " days, " + str(hour) + " hours, " + str(min) + " minutes")
     elif hour > 0:
         print("Your total cooking time is " + str(hour) + " hours, " + str(min) + " minutes")
