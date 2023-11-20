5a67d8e07962d0203326504eb63599a428c5e830
Muffinney <mf101408@wgcloud.org>
2023-11-20 11:11:05 -0600

    Added instructions to README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Turkey-Tracker
+### Version 1.0
+### Project by Matthew Finney
+### Calculates total cooking time for any number of turkeys with a variety of cooking methods
+#### Step 1: Input the number of turkeys being cooked
+#### Step 2: Input weight of the first turkey
+#### Step 3: Input method of cooking for the first turkey
+#### Step 4: Repeat steps 2 and 3 for any additional turkeys
+#### Step 5: Read your total cooking time
+#### Step 6: Answer y to run the program again
--- Turkey.py
+++ Turkey.py
@@ -1,14 +1,3 @@
-cookTim = []
-def convertTim():
-    if min >= 60:
-        hour = (min-(min%60))/60
-        min = min%60
-        if hour >= 24:
-            day = (hour-(hour%24))/24
-            hour = hour%24
-            if day >= 7:
-                week = (day-(day%7))/7
-                day = day%7
 while True:
     min = 0
     hour = 0
@@ -22,43 +11,33 @@ while True:
         method = input("How do you want to cook the turkey? (roast, grill, fry, smoke, or boil) ")
         # calculate cooking time based on method of cooking
         if method == "roast":
-            indivTim = weight * 14
+            min += weight * 14
         elif method == "grill":
-            indivTim = weight * 12
+            min += weight * 12
         elif method == "fry":
-            indivTim = weight * 4
+            min += weight * 4
         elif method == "smoke":
-            indivTim = weight * 35
+            min += weight * 35
         elif method == "boil":
-            indivTim = weight * 13
+            min += weight * 13
         else:
             print("That method of cooking is not supported by this program")
             # ask if user wants to run program again
-            question = input("Would you like to calculate the cooking time of more turkeys? (y/n) ")
-            again = question == "y"
+            question = input("Would you like to try again? (yes/no) ")
+            again = question == "yes"
             if again:
                 continue
             else:
                 break
-        cookTim.append(indivTim)
-    for i in range(numTur):
-        min = cookTim[i]
-        convertTim()
-        if week > 0:
-            print("Turkey " +  is " + str(week) + " weeks, " + str(day) + " days, " + str(hour) + " hours, " + str(min) + " minutes")
-        elif day > 0:
-            print("Your total cooking time is " + str(day) + " days, " + str(hour) + " hours, " + str(min) + " minutes")
-        elif hour > 0:
-            print("Your total cooking time is " + str(hour) + " hours, " + str(min) + " minutes")
-        else:
-            print("Your total cooking time is " + str(min) + " minutes")
-        min = 0
-        hour = 0
-        day = 0
-        week = 0
-    # sum individual cook times
-    min = sum(cookTim)
-    convertTim()
+    if min >= 60:
+        hour = (min-(min%60))/60
+        min = min%60
+        if hour >= 24:
+            day = (hour-(hour%24))/24
+            hour = hour%24
+            if day >= 7:
+                week = (day-(day%7))/7
+                day = day%7
     # print total cook time
     if week > 0:
         print("Your total cooking time is " + str(week) + " weeks, " + str(day) + " days, " + str(hour) + " hours, " + str(min) + " minutes")
@@ -68,8 +47,12 @@ while True:
         print("Your total cooking time is " + str(hour) + " hours, " + str(min) + " minutes")
     else:
         print("Your total cooking time is " + str(min) + " minutes")
+    min = 0
+    hour = 0
+    day = 0
+    week = 0
     # ask if user wants to run program again
-    question = input("Would you like to calculate the cooking time of more turkeys? (y/n) ")
-    stop = question != "y"
+    question = input("Would you like to calculate the cooking time of more turkeys? (yes/no) ")
+    stop = question != "yes"
     if stop:
         break
